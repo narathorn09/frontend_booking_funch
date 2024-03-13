ab451ca86b02ab87e66d7aaa40da9a51ba2aec41
Narathorn Noophum <narathorn.work@gmail.com>
2024-03-13 13:46:24 +0700

    add validation form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,42 +1,80 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { request } from "../config/axios-config";
-import { useSelector, useDispatch } from "react-redux";
-import { setNewToken, removeToken } from "../redux/tokenSlice";
-import {
-  EyeIcon,
-  EyeSlashIcon,
-  HomeModernIcon,
-} from "@heroicons/react/24/outline";
+import { useDispatch } from "react-redux";
+import { setNewToken } from "../redux/tokenSlice";
+import { HomeModernIcon } from "@heroicons/react/24/outline";
+import TextInput from "../components/TextInput";
 
 const LoginView = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const token = useSelector((state) => state.token.value);
 
   const [showPassword, setShowPassword] = useState(false);
+  const [isLoninError, setIsLoninError] = useState(false);
+
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [formErrors, setFormErrors] = useState({ email: "", password: "" });
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: value ? "" : `${name} is required`,
+    }));
+  };
+
+  const checkFormErrors = (prevErrors) => {
+    if (formData.email === "" && formData.password === "") {
+      setFormErrors({
+        email: "Email is required",
+        password: "Password is required",
+      });
+      return true;
+    } else if (formData.email === "") {
+      setFormErrors({
+        ...formErrors,
+        email: "Email is required",
+      });
+      return true;
+    } else if (formData.password === "") {
+      setFormErrors({
+        ...formErrors,
+        password: "Password is required",
+      });
+      return true;
+    }
+    return false;
+  };
+
   const submitFormLogin = async (event) => {
     event.preventDefault();
-
-    // Get form data
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+    
+    if (checkFormErrors()) {
+      return; // Exit the function if there are errors
+    }
 
     try {
-      const response = await request.post("/login", { email, password });
+      const response = await request.post("/login", {
+        email: formData.email,
+        password: formData.password,
+      });
       if (response.status === 200 && response.data) {
         const newToken = response.data.token;
         dispatch(setNewToken({ newToken: newToken }));
         navigate("/calendar");
       }
-
       console.log("Login successful", response.data);
     } catch (error) {
+      setIsLoninError(true);
       console.error("Error logging in:", error);
     }
   };
@@ -45,7 +83,7 @@ const LoginView = () => {
     <div className="min-h-screen px-16 py-16 bg-gradient-to-r from-indigo-500 to-blue-500">
       <div className="md:container md:mx-auto  grid grid-cols-1 md:grid-cols-2 h-full rounded-3xl drop-shadow-2xl">
         <div className="bg-gradient-to-r from-indigo-400 to-blue-500 p-8 hidden md:block rounded-s-3xl h-full">
-          <div className="flex w-fit border border-gray-300 rounded-full p-2">
+          <div className="flex w-fit border border-gray-300 rounded-full p-2 tracking-in-expand">
             <HomeModernIcon className="h-5 w-5 text-white" />
             <h1 className="text-white font-medium ml-2">F-Booking</h1>
           </div>
@@ -54,62 +92,38 @@ const LoginView = () => {
           <div className="flex-column justify-center content-center">
             <h2 className="text-2xl font-semibold text-center mb-4">
               Sign in to F-Booking
-              <div className="">
-                <p className="text-xs break-words">Token: {token}</p>
-                <button
-                  onClick={() => dispatch(removeToken())}
-                  className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
-                >
-                  <p className="text-xs">LOGOUT</p>
-                </button>
+              <div
+                className={`text-red-500 text-xs italic h-1 ${
+                  isLoninError ? "visible" : "invisible"
+                }`}
+              >
+                Email or password incorrect! Please check again.
               </div>
+              <div></div>
             </h2>
             <form onSubmit={submitFormLogin} className="space-y-4">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-                />
-              </div>
-              <div className="relative">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  name="password"
-                  className="mt-1 p-2 w-full border border-gray-300 rounded-md pr-10"
-                />
-                <button
-                  type="button"
-                  className="absolute inset-y-8 right-0 flex items-center px-2 h-6 focus:outline-none"
-                  onClick={togglePasswordVisibility}
-                >
-                  {showPassword ? (
-                    <EyeSlashIcon
-                      className="h-5 w-5 text-gray-500"
-                      aria-hidden="true"
-                    />
-                  ) : (
-                    <EyeIcon
-                      className="h-5 w-5 text-gray-500"
-                      aria-hidden="true"
-                    />
-                  )}
-                </button>
-              </div>
+              <TextInput
+                type="email"
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+                value={formData.email}
+                error={formErrors.email}
+                onChange={handleChange}
+                required={true}
+              />
+              <TextInput
+                type="password"
+                name="password"
+                label="Password"
+                value={formData.password}
+                placeholder="Enter your password"
+                error={formErrors.password}
+                onChange={handleChange}
+                onShowPassword={togglePasswordVisibility}
+                isShowPassword={showPassword}
+                required={true}
+              />
               <div className="flex-col items-center justify-center">
                 <div className="flex items-center justify-end mb-3">
                   <a
@@ -129,12 +143,12 @@ const LoginView = () => {
             </form>
             <div className="flex items-center justify-center mt-4">
               Don't have an account?
-              <a
-                href="#"
+              <button
+                onClick={() =>navigate("/register")}
                 className="ml-2 text-sm text-indigo-500 hover:text-indigo-700 flex justify-self-end"
               >
                 Register now!
-              </a>
+              </button>
             </div>
           </div>
         </div>
--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,17 +1,151 @@
-import { useEffect, useState } from "react";
-import { useAxiosWithToken } from "../config/axios-config";
+import React, { useState } from "react";
+import { request } from "../config/axios-config";
+import TextInput from "../components/TextInput";
 
 const RegisterView = () => {
-  const requestWithToken = useAxiosWithToken();
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       await requestWithToken.get("/user");
-//     };
-//     fetchData();
-//   }, []);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isShowPasswordConfirm, setIsshowPasswordConfirm] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
+  const togglePasswordConfirmVisibility = () => {
+    setIsshowPasswordConfirm(!isShowPasswordConfirm);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    if (name === "confirmPassword") {
+      setFormErrors((prevErrors) => ({
+        ...prevErrors,
+        confirmPassword:
+          value === "" ? `${name} is required ` : value !== formData.password ? "Passwords don't match" : "",
+      }));
+    } else {
+      setFormErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: value ? "" : `${name} is required`,
+      }));
+    }
+  };
+
+  const formReset = () => {
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+    setFormErrors({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await request.post("/register", formData);
+      console.log(response.data); // Log response data
+      formReset();
+    } catch (error) {
+      console.error("Registration failed:", error);
+      // Handle registration error
+    }
+  };
+
   return (
-    <div className="bg-gradient-to-r from-indigo-500 to-blue-500 flex justify-center content-center p-9">
-      <p className="text-center">Register</p>
+    <div className="bg-gradient-to-r from-indigo-500 to-blue-500 min-h-screen flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3"
+      >
+        <p className="text-center text-2xl mb-4">Register</p>
+        <TextInput
+          type="text"
+          name="firstName"
+          label="First name"
+          value={formData.firstName}
+          error={formErrors.firstName}
+          onChange={handleChange}
+          placeholder="Enter your first name"
+        />
+        <TextInput
+          type="text"
+          name="lastName"
+          label="Last name"
+          value={formData.lastName}
+          error={formErrors.lastName}
+          onChange={handleChange}
+          placeholder="Enter your last name"
+        />
+        <TextInput
+          type="email"
+          name="email"
+          label="Email"
+          placeholder="Enter your email"
+          value={formData.email}
+          error={formErrors.email}
+          onChange={handleChange}
+        />
+        <TextInput
+          type="password"
+          name="password"
+          label="Password"
+          placeholder="Enter your password"
+          value={formData.password}
+          error={formErrors.password}
+          onChange={handleChange}
+          onShowPassword={togglePasswordVisibility}
+          isShowPassword={isShowPassword}
+        />
+        <TextInput
+          type="password"
+          name="confirmPassword"
+          label="Confirm Password"
+          placeholder="Confirm your password"
+          value={formData.confirmPassword}
+          error={formErrors.confirmPassword}
+          onChange={handleChange}
+          onShowPassword={togglePasswordConfirmVisibility}
+          isShowPassword={isShowPasswordConfirm}
+        />
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Register
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
